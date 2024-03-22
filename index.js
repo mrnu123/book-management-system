9870815b83df59d1cb0269303962181bd9773f97
anuchit <anuchit.nu567@gmail.com>
2024-03-23 00:31:28 +0700

    add validate price

--- index.js
+++ index.js
@@ -2,15 +2,95 @@ const books = [];
 const readme = () => {
   console.log("Please follow this instruction:");
 };
+
+const validateYear = (year) => {
+  const _yearNow = new Date().getFullYear();
+  if (year == "") return false;
+  try {
+    const _year = Number.parseInt(year);
+    if (_year > _yearNow) {
+      return false;
+    }
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+const validateBookPrice = (price) => {
+  const priceSplitDot = price.split(".");
+  if (priceSplitDot.length > 2) {
+    return false;
+  }
+  const price0 = priceSplitDot[0];
+
+  const priceComma = price0.split(",");
+  console.log(priceComma);
+  let idx = 0;
+  if (priceComma.length > 1) {
+    for (const item of priceComma) {
+      console.log(item);
+      if (priceComma[0].length > 3) {
+        return false;
+      } else if (idx > 0 && item.length != 3) {
+        return false;
+      }
+      idx += 1;
+    }
+  }
+
+  const re = /^[0-9]{1,}.[0-9]{2,}$/;
+  let _price = price.replaceAll(",", "");
+  if (priceSplitDot.length == 1) {
+    _price += ".00";
+  }
+  console.log(_price.match(re));
+  return _price.match(re) != null;
+};
+
 const addBook = () => {
-  let bookTitle = prompt("Enter the title of the book : ");
-  alert(`You have entered ${bookTitle}`);
-  let bookAuthor = prompt("Enter the author of the book :");
-  alert(`The author of ${bookTitle} book is ${bookAuthor}`);
-  let bookYear = prompt("Enter the year of the book :");
-  alert(`The year of the book is ${bookYear}`);
-  let bookPrice = prompt("Enter the price of the book (THB): ");
-  alert(`The price of book is ${bookPrice} THB.`);
+  let bookTitle;
+  let bookAuthor;
+  let bookYear;
+  let bookPrice;
+  let _bookYearIsValid = false;
+  let _bookPriceIsValid = false;
+  do {
+    bookTitle = prompt("Enter the title of the book : ");
+    if (bookTitle == "") {
+      alert("Title book is invalid.");
+    } else {
+      alert(`You have entered ${bookTitle}`);
+    }
+  } while (bookTitle == "");
+  do {
+    bookAuthor = prompt("Enter the author of the book :");
+    if (bookAuthor == "") {
+      alert("Author book is invalid");
+    } else {
+      alert(`The author of ${bookTitle} book is ${bookAuthor}`);
+    }
+  } while (bookAuthor == "");
+
+  do {
+    bookYear = prompt("Enter the year of the book (A.D.):");
+    _bookYearIsValid = validateYear(bookYear);
+    if (_bookYearIsValid) {
+      alert(`The year of the book is ${bookYear}`);
+    } else {
+      alert("The year of the book is invalid.");
+    }
+  } while (!_bookYearIsValid);
+
+  do {
+    bookPrice = prompt("Enter the price of the book (THB): ");
+    _bookPriceIsValid = validateBookPrice(bookPrice);
+    if (_bookPriceIsValid) {
+      alert(`The price of book is ${bookPrice} THB.`);
+    } else {
+      alert("The price of book is invalid.");
+    }
+  } while (!_bookPriceIsValid);
 
   const book = {
     id: books.length + 1,
